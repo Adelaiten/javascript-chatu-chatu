b8316b7f459af844ea36f8ef8a6d9a3b7e84e879
kodoj <kodoj1@o2.pl>
2018-12-19 10:27:02 +0100

    redirecting to mainPage

--- public/js/mainChatPage.js
+++ public/js/mainChatPage.js
@@ -5,3 +5,19 @@ function getUserName() {
 function isUserSignedIn() {
     return !!firebase.auth().currentUser;
 }
+
+function signOut() {
+    firebase.auth().signOut()
+        .then(() => {window.location = '/index.html'});
+}
+
+// function logStatusObserver() {
+//     if(!isUserSignedIn) {
+//         window.location = '/';
+//     }
+// }
+
+var signOutButtonElement = document.getElementById('logout');
+signOutButtonElement.addEventListener('click', signOut());
+
+// logStatusObserver();
