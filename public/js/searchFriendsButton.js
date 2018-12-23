986441bbd966bb6a68d0513265c3215d67a7e507
kodoj <kodoj1@o2.pl>
2018-12-23 19:12:31 +0100

    dev merged with private chat functions

--- public/js/searchFriendsButton.js
+++ public/js/searchFriendsButton.js
@@ -2,49 +2,17 @@ let isMenuOpen = false;
 let usersLoaded = false;
 document.getElementById("menu").addEventListener("click", function(){
     if(!isMenuOpen){
-        let usersSearcher = document.getElementById("friend-searcher");
+        var usersSearcher = document.getElementById("friend-searcher");
         usersSearcher.style.display="block";
         if(!usersLoaded){
-            let usersDatabase = firebase.database().ref('users');
-            usersDatabase.on('value', function(snapshot){
+            var usersDatabase = firebase.database().ref('users');
+            usersDatabase.once('value', function(snapshot){
                 snapshot.forEach(function(childSnapshot){
-                    let friendId = childSnapshot.key;
-                    let user = childSnapshot.val().name;
-                    let usersDiv = document.createElement("div");
-                    usersDiv.setAttribute("id", friendId);
-                    let imageUser = document.createElement("img");
-                    let userDescriptionSpan = document.createElement("span");
-                    let userNameSpan = document.createElement("span");
-                    let addFriendButton = document.createElement("button");
-                    let imageButton = document.createElement("img");
-                    usersDiv.classList.add("user-found");
-                    imageUser.setAttribute("src", "img/dog.png");
-                    
-                    addFriendButton.classList.add("add-friend-button");
-
-                    imageButton.classList.add("add-friend-button");
-                    imageButton.setAttribute("src", "img/plus.svg");
-                    addFriendButton.appendChild(imageButton);
-                    
-                    imageUser.classList.add("user-photo");
-                    userDescriptionSpan.classList.add("user-found-description");
-                    userNameSpan.classList.add("user-found-name");
-                    userNameSpan.textContent = user;
-                    userDescriptionSpan.appendChild(userNameSpan);
-                    usersDiv.appendChild(imageUser);
-                    usersDiv.appendChild(userDescriptionSpan);
-                    usersDiv.appendChild(addFriendButton);
-                    usersSearcher.appendChild(usersDiv);
-
-                    addFriendButton.addEventListener("click", function(){
-                        let currentUser = firebase.auth().currentUser;
-                        let currentUserId = currentUser.uid;
-                    
-                        createPrivateChat(currentUserId, friendId);  
-                    });
+                    createHTMLElement(childSnapshot, usersSearcher);
                     
                 })
             });
+
             usersLoaded = true;
         }
 
@@ -54,4 +22,42 @@ document.getElementById("menu").addEventListener("click", function(){
         isMenuOpen = false;
     }             
 
+
 });
+function createHTMLElement(childSnapshot, usersSearcher){
+    var userId = childSnapshot.key;
+    var user = childSnapshot.val().name;
+
+    // var email = childSnapshot.val().email;
+    var usersDiv = document.createElement("div");
+    // usersDiv.setAttribute("id", userId);
+    var imageUser = document.createElement("img");
+    var userDescriptionSpan = document.createElement("span");
+    var userNameSpan = document.createElement("span");
+    var addFriendButton = document.createElement("button");
+    var imageButton = document.createElement("img");
+    usersDiv.classList.add("user-found");
+    imageUser.setAttribute("src", "img/dog.png");
+    
+    addFriendButton.classList.add("add-friend-button");
+
+    imageButton.classList.add("add-friend-button");
+    imageButton.setAttribute("src", "img/plus.svg");
+    addFriendButton.appendChild(imageButton);
+    
+    imageUser.classList.add("user-photo");
+    userDescriptionSpan.classList.add("user-found-description");
+    userNameSpan.classList.add("user-found-name");
+    userNameSpan.textContent = user;
+    userDescriptionSpan.appendChild(userNameSpan);
+    usersDiv.appendChild(imageUser);
+    usersDiv.appendChild(userDescriptionSpan);
+    usersDiv.appendChild(addFriendButton);
+    usersSearcher.appendChild(usersDiv);
+
+    addFriendButton.addEventListener("click", function(){
+        let currentUser = firebase.auth().currentUser;
+        let currentUserId = currentUser.uid;
+        createPrivateChat(currentUserId, userId);
+    });
+}
